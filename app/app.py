f6e962f7ab9cc8fd8eadf60f3178ccdb1110d302
Heejong Ahn <heejongahn@gmail.com>
2015-06-28 16:15:00 +0900

    Login/Logout Functionalities

--- app/app.py
+++ app/app.py
@@ -73,6 +73,45 @@ class FTSEntry(FTSModel):
     class Meta:
         database = database
 
+
+# Login / Logout --------------------------------------------------------------
+
+## Decorator ensuring the user is logged in
+def login_required(fn):
+    @functools.wraps(fn)
+    def inner(*args, **kwargs):
+        # If logged in, just execute decorated function with args
+        if session.get('logged_in'):
+            return fn(*args, **kwargs)
+
+        # If not logged in, redirect to login page
+        return redirect(url_for('login', next=request.path))
+    return inner
+
+## Login Function
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    next_url = request.args.get('next') or request.form.get('next')
+    if request.method == 'POST' and request.form.get('password'):
+        password = request.form.get('password')
+        if password == app.config['ADMIN_PASSWORD']:
+            session['logged_in'] = True
+            session.permanent = True  # Use cookie to store session.
+            flash('You are now logged in.', 'success')
+            return redirect(next_url or url_for('index'))
+        else:
+            flash('Incorrect password.', 'danger')
+    return render_template('login.html', next_url=next_url)
+
+## Logout Function
+@app.route('/logout/', methods=['GET', 'POST'])
+def logout():
+    if request.method == 'POST':
+        session.clear()
+        return redirect(url_for('login'))
+    return render_template('logout.html')
+
+
 # Initialization Codes --------------------------------------------------------
 
 ## Template Filter
